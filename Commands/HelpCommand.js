322f5c9427a151130301f0a3d7d31bf9d683cb49
Austin Lewis <austin.lew001@gmail.com>
2019-12-06 09:39:02 -0700

    updated help command

--- Commands/HelpCommand.js
+++ Commands/HelpCommand.js
@@ -9,14 +9,7 @@ var textFile = fs.readFileSync('./about', 'utf8');
 
 exports.helpMessage = function (args, msg) {
 
-    msg.channel.send("`I AM BOYO! THE GREAT WIZARD OF DUNGEONS & DRAGONS` \n\
-This `HELP MESSAGE` will be updated as commands are added! ");
-    
-    if(msg.member.hasPermission("ADMINISTRATOR")) {
-        msg.channel.send(msg.author + " is an ADMIN!");
-    } else {
-        msg.channel.send(msg.author + " is not an ADMIN");
-    };
+    msg.channel.send("Please refer to 'https://github.com/DxRKENEDSKY/BoyoBot-FullRelease' for a full list of commands. " );
 
 };
 
