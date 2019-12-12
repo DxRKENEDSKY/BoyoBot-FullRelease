f1fc93a4fa5748295a4838b368b9e3ab6a4ca2bc
DxRKENEDSKY <52719530+DxRKENEDSKY@users.noreply.github.com>
2019-12-12 09:09:39 -0700

    Update main.js

--- main.js
+++ main.js
@@ -80,7 +80,7 @@ function processCommand(msg) {
 
 exports.reset = function() {
     client.destroy();
-    client.login('NjUxNDYxODI3MjUwNjE4Mzg3.XfFDaw.MKxhMH-j_rOAfeD65z_zFol0mUg');
+    client.login(process.env.BOT_TOKEN);
 };
 
-client.login('NjUxNDYxODI3MjUwNjE4Mzg3.XfFDaw.MKxhMH-j_rOAfeD65z_zFol0mUg');
+client.login(process.env.BOT_TOKEN);
